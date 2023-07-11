2996a84fcd79c84f8d46d87a8048a34e8931b8cb
HuuTai Tran <htran@lm0019.home>
2023-07-11 18:52:26 +0200

    add route get curretn user

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
         post '/login', to: 'users/sessions#create'
         delete '/logout', to: 'users/sessions#destroy'
       end
+      namespace :users do
+        resource :currents, only: [:show]
+      end
       resources :users, only: [:create]
       resources :raw_materials, only: [:index, :show, :create, :update, :destroy]
     end
